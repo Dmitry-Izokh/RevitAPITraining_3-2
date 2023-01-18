7c4289f64796d66f9ddd978db9422dc81c4297df
Дмитрий Изох <izokh_dv@mail.ru>
2023-01-18 22:34:05 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_3-2/Main.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_3_2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public string massage = "Количество воздуховодов";
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+            
+            List<FamilyInstance> fInstances = new FilteredElementCollector(doc)
+                .OfCategory(BuiltInCategory.OST_DuctCurves)
+                .WhereElementIsNotElementType()
+                .Cast<FamilyInstance>()
+                .ToList();
+            
+            TaskDialog.Show(massage, fInstances.Count.ToString());
+            return Result.Succeeded;
+        }
+
+    }
+
+}
+
